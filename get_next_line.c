9c12d13b790efc9918a8cc3c773c45fad0613c65
Ruth Canchanya Hurtado <rcanchan@c2r9s5.42madrid.com>
2024-11-01 17:06:28 +0100

    GNL V1

---
+++ get_next_line.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcanchan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/18 14:00:35 by rcanchan          #+#    #+#             */
+/*   Updated: 2024/10/18 14:05:26 by rcanchan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*read_lines(int fd, char *store_lines, char *buffer)
+{
+	ssize_t	number_bytes;
+	char	*temporal_store;
+
+	number_bytes = 1;
+	while (number_bytes > 0)
+	{
+		number_bytes = read(fd, buffer, BUFFER_SIZE);
+		if (number_bytes == -1)
+		{
+			free(store_lines);
+			return (NULL);
+		}
+		else if (number_bytes == 0)
+			break ;
+		buffer[number_bytes] = 0;
+		if (!store_lines)
+			store_lines = ft_strdup("");
+		temporal_store = store_lines;
+		store_lines = ft_strjoin(temporal_store, buffer);
+		free(temporal_store);
+		temporal_store = NULL;
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	return (store_lines);
+}
+
+static char	*extrac_line(char *line)
+{
+	char	*store_lines;
+	ssize_t	i;
+
+	i = 0;
+	while (line[i] != '\n' && line[i] != '\0')
+		i++;
+	if (line[i] == 0 || line[1] == 0)
+		return (NULL);
+	store_lines = ft_substr(line, i + 1, ft_strlen(line) - i);
+	line[i + 1] = 0;
+	return (store_lines);
+}
+
+char	*get_next_line(int fd)
+{
+	char			*buffer;
+	static char		*store_lines;
+	char			*line;
+
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (NULL);
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	{
+		free(store_lines);
+		free(buffer);
+		store_lines = NULL;
+		buffer = NULL;
+		return (NULL);
+	}
+	line = read_lines(fd, store_lines, buffer);
+	free(buffer);
+	buffer = NULL;
+	if (!line)
+		return (NULL);
+	store_lines = extrac_line(line);
+	return (line);
+}
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	fd = open("hola.txt", O_RDONLY);
+	if (fd < 0)
+	{
+		perror("Error opening file");
+		return (1);
+	}
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("%s", line);
+		free(line);
+	}
+	close(fd);
+	return (0);
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcanchan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 18:09:26 by rcanchan          #+#    #+#             */
+/*   Updated: 2024/10/21 18:21:14 by rcanchan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	c;
+
+	c = 0;
+	while (s[c] != '\0')
+		c++;
+	return (c);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[c] != '\0')
+	{
+		if (s[i] == (unsigned char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if ((unsigned char)c == '\0')
+		return ((char *)&s[i]);
+	return (0);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	count;
+
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		len = 0;
+	if (ft_strlen(s) - start < len)
+		len = ft_strlen(s) - start;
+	str = (char *)malloc((len + 1) * sizeof(char));
+	count = 0;
+	if (!str)
+		return (NULL);
+	while (count < len)
+	{
+		str[count] = s[start + count];
+		count++;
+	}
+	str[count] = '\0';
+	return (str);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	if (s1 == NULL && s2 == NULL)
+		return (free(s1), NULL);
+	i = 0;
+	j = 0;
+	str = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (str == NULL)
+		return (free(s1), NULL);
+	while (s1 != NULL && s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2 != NULL && s2[j] != '\0')
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	free(s1);
+	return (str);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*dst;
+	char	*begin;
+
+	dst = (char *)malloc(ft_strlen(s1) + 1);
+	if (dst == NULL)
+		return (NULL);
+	begin = dst;
+	while (*s1 != '\0')
+		*dst++ = *s1++;
+	*dst = '\0';
+	return (begin);
+}
